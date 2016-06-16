4c59d56a1a9873ae96c3cc0be2190bd5c356e48c
co_rel <nikolai.korniichuk@gmail.com>
2016-06-17 02:40:25 +0300

    comments and checking if file is reachable

--- UDPServerApp_Listener/Program.cs
+++ UDPServerApp_Listener/Program.cs
@@ -39,8 +39,8 @@ namespace UDPServerApp_Listener
 
     class Program
     {
-        static int ListeningPort = 5000;
-        static string path = @"data.bin";
+        static int ListeningPort = 5000; //port will be used by default
+        static string path = @"data.bin"; //file will be created in the root folder of application
 
         static void Main()
         {
@@ -48,40 +48,86 @@ namespace UDPServerApp_Listener
             var _port = Console.ReadLine();
             if (_port != null && _port != "")
                 ListeningPort = int.Parse(_port);
-            
+
             Thread receiveThread = new Thread(new ThreadStart(Receiver));
-            receiveThread.Start();
-            //code of Main method will work in its thread without corrupting Receiver thread             
+            receiveThread.Start();//receiving a message will be performed in separate thread for supporting of asynchronicity
+            //code of Main method will work in its thread without corrupting Receiver thread
         }
+
 
         public static void Receiver()
         {
             UdpClient receiver = new UdpClient(ListeningPort);
             IPEndPoint remoteEP = null; //application will listen all connections but only to specified port of UDPClient
             Console.WriteLine("Server is listening on port "+ListeningPort.ToString()+"...");
+            BinaryFormatter formatter = new BinaryFormatter(); //Binary serializator
             try
             {
-                BinaryFormatter formatter = new BinaryFormatter(); //Binary serialization
                 while (true)
                 {
                     TempData temp = new TempData();
                     using (var ms = new MemoryStream(receiver.Receive(ref remoteEP)))
                     {
                         temp = formatter.Deserialize(ms) as TempData;
-                        File.AppendAllText(path, temp.ToString());
-                        Console.WriteLine("New message received: \n"+temp.ToString());
+                        if(FileAvailable(path)) //check if file stream can be opened for writing 
+                            //all the received messages will be added to file if access will be
+                            //fixed in less then 10 seconds.
+                            File.AppendAllText(path, temp.ToString()); //writing data to file
+                        else
+                        {
+                            throw new Exception("Message is received. But file cannot be opened more then 10 seconds.");
+                        }
+                        Console.WriteLine("New message received: \n" + temp.ToString());
                     }
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                Console.ReadKey();
             }
             finally
             {
-                receiver.Close(); //closing of UDP Socket in case of crash while receiving data
+                receiver.Close();
             }
+        }
+
+        public static bool FileAvailable(string path)
+        {
+            /*
+             function checks whether file is busy at the moment or not.
+             if file cannot be opened, function wait for a second and tries again.
+             Whole number of tries is 10. Function is created for synchronisation of threads.
+             Monitor would be better on my oppinion but in case of using Monitor
+             in main class we need to perform additional lock opening new thread.
+             So due to fact that receiveThread should be standalone thread such approach is 
+             more suitable.
+             */
+            int numTries = 0;
+            while (true)
+            {
+                try
+                {
+                    using(FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        fs.ReadByte();
+                        break;
+                    }
+                }
+                catch (IOException)
+                {
+                    if (++numTries >= 10)
+                    {
+                        return false;
+                    }
+                    else
+                        Thread.Sleep(1000);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }            
+            return true;
         }
     }
 }
